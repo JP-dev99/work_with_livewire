baefcdc9483c3387dd9df728e4cae90ccde19c2c
João Pedro G <joaocitimo@gmail.com>
2022-01-05 10:45:08 -0300

    first_commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title> Livewire SON </title>
+    </head>
+
+    @livewireStyles
+
+<body>
+    @livewire('hello-world')
+    @livewire('data-binding')
+
+    @livewireScripts
+</body>
+</html>
